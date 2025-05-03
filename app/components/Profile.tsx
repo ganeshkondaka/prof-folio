bf88d2b7654ad962162fab6170882e24ccfd70d4
ganeshkondaka <kondakaganesh2003@outlook.com>
2025-05-03 21:54:04 +0530

    updated bio

--- app/components/Profile.tsx
+++ app/components/Profile.tsx
@@ -16,7 +16,7 @@ export default function Profile() {
         <div className='flex flex-row justify-center items-center px-4 pt-8 pb-5 xl:pb-4 md:py-14 max-w-5xl w-auto'>
             <div className='w-80 md:w-auto'>
                 <h2 className='text-2xl font-bold md:text-6xl px-2'>Hi,I'm Ganesh 👋</h2>
-                <p className='text-zinc-300 text-sm md:text-xl p-2'>Aspiring Fullstack dev and Blending Web dev, AI, and open source to build, break, and innovate!</p>
+                <p className='text-zinc-300 text-sm md:text-xl p-2'>A Fullstack dev , Blending Web dev and AI, and open source to build, break, and innovate!</p>
                 <div className='flex flex-col lg:flex-row justify-between  lg:gap-2'>
                     <div className="relative " id="contact">
                         <Dock iconMagnification={60} iconDistance={100} className='border border-zinc-700'>
@@ -61,7 +61,7 @@ export default function Profile() {
                 </div>
             </div>
             <div className='w-auto'>
-                <Image src="/ganesh.png" alt="Ganesh" width={150} height={150} className='aspect-square rounded-3xl border p-1 border-zinc-400 hover:scale-110 translate duration-200 ease-in-out' />
+                <Image src="/ganesh.png" alt="Ganesh" width={150} height={150} className='aspect-square rounded-3xl border p-1 border-zinc-700 hover:scale-110 translate duration-200 ease-in-out' />
             </div>
         </div>
     )
