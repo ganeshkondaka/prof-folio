71eeeaa6d5bc4b5c37f62901f62c0c33211e560b
ganeshkondaka <kondakaganesh2003@outlook.com>
2025-07-10 15:52:27 +0530

    added grid pattern

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,14 @@ import Workexp from "./components/Workexp";
 import Footer from "./components/Footer";
 import GitHubCalendar from "react-github-calendar";
 import AnimatedSection from "./components/AnimatedSection";
+import { GridPattern } from "@/components/magicui/grid-pattern";
 
 export default function Home() {
   return (
     <div className="relative">
       <div className="absolute inset-0">
         <DotPattern className="w-full h-full" />
+        {/* <GridPattern className="w-full h-full" /> */}
       </div>
       <div className="overflow-hidden flex justify-center md:mx-64 items-center">
         <div className="z-10 flex flex-col justify-center items-center w-full">
